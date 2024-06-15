d292a1b7cfc353dec5503e1d4c9169ad804ba917
anatharias <anatharias@gmail.com>
2024-06-14 22:08:36 -0400

    modified:   control.py

--- control.py
+++ control.py
@@ -1,69 +1,34 @@
-from gpiozero import LED, Button
+# control.py
 
-class Control:
-    def __init__(self, sensor):
-        self.relay = LED(17)
-        self.button1 = Button(2)
-        self.button2 = Button(3)
-        self.button3 = Button(4)
-        self.button4 = Button(5)
-        self.sensor = sensor
+import time
+import RPi.GPIO as GPIO
 
-        self.button1.when_pressed = self.handle_button1_press
-        self.button2.when_pressed = self.handle_button2_press
-        self.button3.when_pressed = self.handle_button3_press
-        self.button4.when_pressed = self.handle_button4_press
+# Configuration des pins GPIO pour le relais
+RELAY_PIN = 18  # Par exemple
 
-    def handle_button1_press(self):
-        if not self.relay.is_active:
-            self.start_system()
-        else:
-            self.stop_pump()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
 
-    def handle_button2_press(self):
-        if not self.relay.is_active and not self.has_sunlight():
-            self.run_pump_for(5)
-            self.evaluate_conditions()
+def relay_on():
+    GPIO.output(RELAY_PIN, GPIO.HIGH)
+    print("Relais activé")
 
-    def handle_button3_press(self):
-        self.stop_system()
+def relay_off():
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    print("Relais désactivé")
 
-    def handle_button4_press(self):
-        self.transmit_data()
+def control_loop():
+    try:
+        while True:
+            # Ajouter la logique pour le contrôle du relais
+            # Exemple basique pour activer et désactiver le relais
+            relay_on()
+            time.sleep(5)  # Relais activé pendant 5 secondes
+            relay_off()
+            time.sleep(5)  # Relais désactivé pendant 5 secondes
 
-    def start_system(self):
-        self.relay.on()
-        # Log system start
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
-    def stop_pump(self):
-        self.relay.off()
-        # Log pump stop
-
-    def stop_system(self):
-        self.relay.off()
-        # Log system stop
-        self.shutdown()
-
-    def run_pump_for(self, minutes):
-        self.relay.on()
-        time.sleep(minutes * 60)
-        self.relay.off()
-
-    def has_sunlight(self):
-        data = self.sensor.read_all()
-        return data["light_intensity"] > 10000
-
-    def evaluate_conditions(self):
-        data = self.sensor.read_all()
-        if data["temp_entree"] <= data["temp_air"] + 0.5:
-            self.relay.on()
-        else:
-            self.relay.off()
-
-    def transmit_data(self):
-        # Log data transmission
-        pass
-
-    def shutdown(self):
-        # Perform shutdown tasks
-        pass
+if __name__ == "__main__":
+    control_loop()
